3eaf66e9ed360e5675981f0062d19a2b66cc5766
spider4216 <spider4216@gmail.com>
2015-08-29 15:57:12 +0600

    Refactoring 1 fix

--- core/modules/install/assets/js/script.js
+++ core/modules/install/assets/js/script.js
@@ -1,4 +1,39 @@
 function installProcess(e) {
-    $.blockUI();
-    console.log('install');
+    var form = $('form');
+    var url = form.attr('action');
+    var formData = form.serialize();
+    $.ajax({
+        type: 'post',
+        url: url,
+        data: formData,
+        dataType: 'json',
+        beforeSend: function() {
+            $(window).block({
+                message : '<div class="progress"><div class="progress-bar progress-bar-striped active" ' +
+                'role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">' +
+                '<span class="sr-only"></span></div></div><span>Loading</span>',
+                css: {
+                    'border' : 'none',
+                    'padding' : '10px'
+                }
+            });
+        },
+        success: function(result) {
+            if ('err' == result.status) {
+                $.notify(result.text, "error");
+            }
+
+            if ('ok' == result.status) {
+                $.notify(result.text, "success");
+                setTimeout(function() {
+                    window.location.href = '/install/setup/finish';
+                }, 500);
+            }
+
+            console.log(result);
+        },
+        complete: function() {
+            $(window).unblock();
+        }
+    });
 }
